454a9912c4a2fe650cb1512b8b5ea7b0232e261d
Jaswinder <jaswinder@gradright.com>
2026-06-07 17:48:58 +0530

    icon fix

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class JobSyncApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'JobPulse AI',
+      title: 'JobPulseAI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
